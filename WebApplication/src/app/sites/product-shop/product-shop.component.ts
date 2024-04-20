41623f844596649e00b7977b26e9683ca5643c12
Kubus <jakub.kangowski7@vp.pl>
2024-04-20 17:07:13 +0200

    dropdown menu works, filtering products also

--- WebApplication/src/app/sites/product-shop/product-shop.component.ts
+++ WebApplication/src/app/sites/product-shop/product-shop.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {CommonModule} from "@angular/common";
+import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {ProductService} from "../../product/product.service";
 import {Product} from "../../product/product";
 import {RouterLink, RouterLinkActive} from "@angular/router";
@@ -9,14 +9,15 @@ import {FormsModule} from "@angular/forms";
 @Component({
   selector: 'app-product-shop',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule ],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule, NgOptimizedImage],
   templateUrl: './product-shop.component.html',
   styleUrl: './product-shop.component.css'
 })
 export class ProductShopComponent implements OnInit {
-
   products: Product[];
   uniqueCategories: string[];
+  selectedCategory: string;
+  filteredProducts: Product[];
 
   constructor(private productService: ProductService) { }
 
@@ -24,6 +25,8 @@ export class ProductShopComponent implements OnInit {
     this.productService.findAll().subscribe(data => {
       this.products = data;
       this.getUniqueCategories();
+      this.selectedCategory = 'Wszystko'; // Set default category
+      this.filterProducts();
     });
   }
 
@@ -31,7 +34,19 @@ export class ProductShopComponent implements OnInit {
     const categoriesSet = new Set<string>();
     this.products.forEach(product => categoriesSet.add(product.category));
     this.uniqueCategories = Array.from(categoriesSet);
-    this.uniqueCategories.push("Wszystko")
+    this.uniqueCategories.push("Wszystko");
   }
 
+  onCategoryChange(category: string) {
+    this.selectedCategory = category;
+    this.filterProducts();
+  }
+
+  filterProducts() {
+    if (this.selectedCategory === 'Wszystko') {
+      this.filteredProducts = this.products; // Show all products
+    } else {
+      this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
+    }
+  }
 }
