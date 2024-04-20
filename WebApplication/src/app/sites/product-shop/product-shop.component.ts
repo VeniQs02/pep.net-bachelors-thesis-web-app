9c4749ba216b51d0e2bfd3fd13b234df1227bb34
Kubus <jakub.kangowski7@vp.pl>
2024-04-20 15:02:52 +0200

    dropdown menu for product-shop.component, minor responsivity fixes

--- WebApplication/src/app/sites/product-shop/product-shop.component.ts
+++ WebApplication/src/app/sites/product-shop/product-shop.component.ts
@@ -3,24 +3,35 @@ import {CommonModule} from "@angular/common";
 import {ProductService} from "../../product/product.service";
 import {Product} from "../../product/product";
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-product-shop',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule ],
   templateUrl: './product-shop.component.html',
   styleUrl: './product-shop.component.css'
 })
-export class ProductShopComponent  implements OnInit{
+export class ProductShopComponent implements OnInit {
 
   products: Product[];
+  uniqueCategories: string[];
 
-  constructor(private productService: ProductService) {
-  }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.productService.findAll()
-      .subscribe(data => this.products = data);
+    this.productService.findAll().subscribe(data => {
+      this.products = data;
+      this.getUniqueCategories();
+    });
+  }
+
+  getUniqueCategories() {
+    const categoriesSet = new Set<string>();
+    this.products.forEach(product => categoriesSet.add(product.category));
+    this.uniqueCategories = Array.from(categoriesSet);
+    this.uniqueCategories.push("Wszystko")
   }
 
 }
